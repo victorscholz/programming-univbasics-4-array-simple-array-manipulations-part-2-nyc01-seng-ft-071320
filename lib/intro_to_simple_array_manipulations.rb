3d0bf7c66d13e3e95edfd023c2ea41709bb2385b
impartial-genre-2648 <vgscholz@gmail.com>
2020-07-06 03:13:29 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,24 @@
+def using_concat(arr1, arr2)
+  arr1.concat(arr2)
+end
+
+def using_insert(arr1, element)
+  arr1.insert(4, element)
+end 
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten 
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete_at(integer)
+end
+  
